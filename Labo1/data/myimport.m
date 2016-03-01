2f008468b22872c9857c1f5ab9dcda5c3c058893
Philippe Verbist <verbist.philippe@gmail.com>
2016-03-01 10:22:18 +0100

    modify myimport

--- Labo1/data/myimport.m
+++ Labo1/data/myimport.m
@@ -1,5 +1,15 @@
 function myimport
+% t [s] || setpoint level [cm] || tank level [cm] || initial flow u0 [ml.s^-1] || command PI (du) [ml.s^-1]
+
 filename = 'Test_1 copy.txt';
 A = (importdata(filename));
-plot(A(:,1),A(:,3));
+plot(A(:,1),A(:,2),'r*');
+hold on;
+plot(A(:,1),A(:,3),'b-');
+hold on;
+plot(A(:,1),A(:,4),'g-');
+hold on;
+plot(A(:,1),A(:,5),'y*');
+
+
 end
