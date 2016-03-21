5d7c64e5a8f015d520b9600f7e4deaee6b01a606
Philippe Verbist <verbist.philippe@gmail.com>
2016-03-21 22:26:48 +0100

    some handling of data

--- labo2/data/calc_tau.m
+++ labo2/data/calc_tau.m
@@ -1,15 +1,4 @@
 clear all;
-res = importdata('Z:\LINMA1510\Test_1.txt')
-factor = 0.63;
-diff = res(150,5)-res(70,5)
-base = res(70,5)
+res = importdata('1_step_response_done.txt'):
 
-
-
-for i=70:150
-    if res(i,5)>base+diff*factor
-        res(i,1)
-        i
-        break
-    end
-end
+plot(res(:,1), res(:,5));
