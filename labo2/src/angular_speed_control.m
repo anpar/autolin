d41f97bf11be95fde5028d01811964ce1607e53b
anpar <paris.antoine.paris@gmail.com>
2016-03-21 23:50:41 +0100

    Beginning of report for lab2

---
+++ labo2/src/angular_speed_control.m
@@ -0,0 +1,14 @@
+tau = 1.5;
+K= 2.1917;
+format long;
+syms K1 K0 xsi
+
+xsi = eval(solve(pi*xsi + log(0.02)*sqrt(1-xsi^2),xsi))
+% passage 4 à 16
+
+[K0, K1] = solve(xsi-(K1*K+1)/(2*sqrt(K0*K*tau)),4/(xsi*sqrt(K*K0/tau))-0.90*4*tau, K0, K1);
+K0=eval(K0)
+K1=eval(K1)
+
+Tr = tf([K0], [tau (K1*K + 1) K0*K]);
+step(Tr)
---
+++ labo2/src/model_identification.m
@@ -0,0 +1,38 @@
+clear all;
+res = importdata('data/model_identification_ultimate.txt');
+
+% Plot the entire data set
+%plot(res(:,1), res(:,5))
+%title('Step response in open loop')
+%xlabel('time [s]');
+%ylabel('angular speed [rot/s]');
+%xlim([res(1,1) res(end,1)])
+
+% Plot the steady state after 6V unit step
+%plot(res(125:220,1), res(125:220,5));
+
+steady_state = mean(res(125:200, 1));
+K = (steady_state-3)/6
+
+start = 0;
+for i=1:length(res)
+    if res(i,5) > 3 & start == 0
+        start = res(i,1);
+    end
+   
+    if res(i,5) > 3 + (steady_state - 3)*0.95
+        tau = (res(i,1) - start)/3
+        break
+    end
+end
+
+Gs = tf([K], [tau 1])
+
+% Confront model and measurements
+%plot(res(87:220,1)-res(87,1), res(87:220,5), 'r'); hold on;
+%[y, t] = step(Gs*6);
+%plot(t, 3+y, 'g');
+%title('Step response in open loop : model vs. measurements')
+%xlabel('time [s]');
+%ylabel('angular speed [rot/s]');
+%legend('Measurements', 'Model');
