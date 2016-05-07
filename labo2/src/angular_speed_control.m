f527ae556286f5b9b9caddb43a1a4549bc6866b8
pverbist95 <philippe.verbist@student.uclouvain.be>
2016-05-07 13:51:21 +0200

    first quick review

--- labo2/src/angular_speed_control.m
+++ labo2/src/angular_speed_control.m
@@ -15,7 +15,7 @@ K = 2.1917;
 
 syms K1 K0 xsi
 xsi = eval(solve(pi*xsi + log(0.02)*sqrt(1-xsi^2),xsi))
-[K0, K1] = solve(xsi-(K1*K+1)/(2*sqrt(K0*K*tau)),4/(xsi*sqrt(K*K0/tau))-0.90*4*tau, K0, K1);
+[K0, K1] = solve(xsi-(K1*K+1)/(2*sqrt(K0*K/tau)),4/(xsi*sqrt(K*K0/tau))-0.90*4*tau, K0, K1);
 
 K0=eval(K0)
 K1=eval(K1)
